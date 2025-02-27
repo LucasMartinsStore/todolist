8f3694480bb46ad8ade3807d744071bb0d600166
Lucas Martins <lucasifbaeuna@gmail.com>
2025-02-27 20:08:35 -0300

    chore: install daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ['dark'],
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
